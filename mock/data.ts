962fcef80a83d156678a51a76665ff9498f7c7c6
sam1code <samcse95@gmail.com>
2021-09-25 10:37:43 +0530

    mock data file adding success

---
+++ mock/data.ts
@@ -0,0 +1,50 @@
+export const Data = [
+  {
+    imgSrc: "https://i.ibb.co/K6hzdB6/Rectangle-5.png",
+    title: "What if famous brands had regular fonts? Meet RegulaBrands!",
+    description:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    profilePic: "https://i.ibb.co/m4Fx26Q/Rectangle-3.png",
+    auther: "Sarthak Kamra",
+    views: "1.4k views",
+    tag: "✍️ Article",
+    first: true,
+  },
+  {
+    imgSrc: "https://i.ibb.co/QNznMjD/Rectangle-5-2.png",
+    title: "What if famous brands had regular fonts? Meet RegulaBrands!",
+    description:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    profilePic: "https://i.ibb.co/rMrDncq/Rectangle-3-1.png",
+    auther: "Sarah West",
+    views: "1.4k views",
+    tag: "🔬️ Education",
+  },
+  {
+    imgSrc: "https://i.ibb.co/s91JzZq/Rectangle-5-3.png",
+    title: "What if famous brands had regular fonts? Meet RegulaBrands!",
+    description:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    profilePic: "https://i.ibb.co/XWHvJ9Z/Rectangle-3-2.png",
+    auther: "Ronal Jones",
+    views: "1.4k views",
+    tag: "✍️ Article",
+    btnData: "visit Website",
+  },
+  {
+    imgSrc: "",
+    btnData: "Apply on Timesjobs",
+    title: "What if famous brands had regular fonts? Meet RegulaBrands!",
+    description:
+      "I’ve worked in UX for the better part of a decade. From now on, I plan to rei…",
+    profilePic: "https://i.ibb.co/25Dyx6B/Rectangle-3-3.png",
+    auther: "Joseph Gray",
+    views: "1.4k views",
+    tag: "✍️ Article",
+  },
+];
+export const MainBannerData = {
+  image: "https://i.ibb.co/pQCVJZt/Rectangle-2.png",
+  title: "Computer Engineering",
+  description: "142,765 Computer Engineers follow this",
+};
